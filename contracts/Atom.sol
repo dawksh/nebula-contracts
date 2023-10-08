d8104af41ececda61ed3ba8ea79d66115d806d55
Daksh <akshd@outlook.com>
2023-10-08 14:09:32 +0530

    ✨ feat: add atom spec

---
+++ contracts/Atom.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.19;
+
+contract Element {
+    address internal _nebula;
+    address internal _issuer;
+
+    error InvalidCaller();
+
+    constructor(address __nebula) {
+        _nebula = __nebula;
+        _issuer = msg.sender;
+    }
+
+    modifier onlyNebula() {
+        if (msg.sender != _nebula) revert InvalidCaller();
+        _;
+    }
+
+    function bond(bytes32 _data) external onlyNebula returns (bool) {
+        //  TODO
+    }
+
+    function unbond(bytes32 _data) external onlyNebula returns (bool) {
+        //  TODO
+    }
+
+    function verifyBond(bytes32 _data) external returns (bool) {
+        //  TODO
+    }
+
+    function issuer() external view returns (address) {
+        return _issuer;
+    }
+
+    function nebula() external view returns (address) {
+        return _nebula;
+    }
+}
