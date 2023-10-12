4d64f2fb72f741066591b68967f5e21e777419fb
Daksh <akshd@outlook.com>
2023-10-12 22:29:26 +0530

    chore: add atom registry

--- contracts/AtomRegistry.sol
+++ contracts/AtomRegistry.sol
@@ -3,11 +3,14 @@
 pragma solidity ^0.8.19;
 
 contract Registry {
+    event AtomRegistered(bytes32 indexed uid, address indexed atom);
+
     mapping(bytes32 => address) internal uidToAddressMap;
 
     function register(address atom) external returns (bytes32) {
         bytes32 uid = keccak256(abi.encodePacked(atom, msg.sender));
         uidToAddressMap[uid] = atom;
+        emit AtomRegistered(uid, atom);
         return uid;
     }
 
