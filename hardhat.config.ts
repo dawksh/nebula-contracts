f02e4046060104a69cacb7cf1c4d67fa3631169a
Daksh <akshd@outlook.com>
2023-10-14 16:31:58 +0530

    feat: add new networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,13 +10,34 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: process.env.SEPOLIA_URL as string,
       accounts: [process.env.PRIVATE_KEY as string],
+    },
+    zkEVM: {
+      url: process.env.ZKEVM_URL as string,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    goerli: {
+      url: process.env.GOERLI_URL as string,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    optimismGoerli: {
+      url: process.env.OPTIMISM_GOERLI_URL as string,
+      accounts: [process.env.PRIVATE_KEY as string],
     }
   },
   etherscan: {
     apiKey: {
       sepolia: process.env.ETHERSCAN_KEY as string,
-    }
-  }
+      zkEVM: process.env.POLYGONSCAN_KEY as string,
+      optimisticGoerli: process.env.OPTIMISM_KEY as string,
+    },
+    customChains: [
+      {
+        network: "zkEVM",
+        chainId: 1422,
+        urls: { apiURL: "https://api-testnet-zkevm.polygonscan.com/api", browserURL: "https://testnet-zkevm.polygonscan.com/" }
+      }
+    ]
+  },
 };
 
 export default config;
